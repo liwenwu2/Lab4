5cf891c267c82e39b7137a72796dec4ccbe84942
william_wu <william_wu@DESKTOP-MB52UEP>
2017-09-27 19:39:15 -0500

    commit

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,43 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/** the javadoc of public class ReclamationProject.
+ *
+ */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+public class ReclamationProject {
+    /** a function gaining the common part of two strings.
+     * @param a the first string.
+     * @param b the second string.
+     * @return the common part.
+     */
+
+    public static String doit(final String a, final String b) {
+        String lager;
+        String smaller;
+        String output = " ";
+        if (a.length() > b.length()) {
+            lager = a;
+            smaller = b;
+        } else {
+            lager = b;
+            smaller = a;
+        }
+        if (a.equals(b)) {
+            return a;
+        }
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < smaller.length(); i++) {
+            for (int j = smaller.length() - i; j > 0; j--) {
+                for (int k = 0; k < lager.length() - j; k++) {
+                     if (smaller.regionMatches(i, lager, k, j) && j > output.length()) {
+                         output = smaller.substring(i, i + j);
+                     }
+                }
+            }
+        }
+        return output;
+    }
 }
